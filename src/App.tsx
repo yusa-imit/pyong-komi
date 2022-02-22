7233015e47a137a7ad1dece15e0bbad7b0264aa2
yusa-imit <esimileiar81@gmail.com>
2022-02-22 23:51:53 +0900

    push all

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import countapi from "countapi-js";
+import axios, { AxiosResponse } from "axios";
+import KOKOMI_IMAGE from "./images/kokomi_no_loop.webp";
+import useWindowSize from "./useWindowSize";
+
+interface formatData {
+    value: number;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const [value, setValue] = useState(-1);
+    const windowSize = useWindowSize();
+    const [clicked, setClicked] = useState(false);
+    useEffect(() => {
+        async function get() {
+            return await axios.get<formatData>(
+                "https://api.countapi.xyz/get/kokomi.sutora.org/1e72fd45-9e3d-422a-8559-4ebf25288917"
+            );
+        }
+        get().then((res) => {
+            setValue(res.data.value);
+        });
+    }, []);
+    useEffect(() => {
+        setTimeout(() => {
+            setClicked(false);
+        }, 50);
+    }, [clicked]);
+    return (
+        <div
+            className="App"
+            onClick={() => {
+                setClicked(true);
+                axios.get<formatData>("https://api.countapi.xyz/hit/kokomi.sutora.org/1e72fd45-9e3d-422a-8559-4ebf25288917")
+                .then((res)=>{
+                    setValue(res.data.value);
+                })
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <div
+                className="kokomi-container"
+                style={{
+                    height:
+                        windowSize.height > windowSize.width
+                            ? "auto"
+                            : windowSize.height * 0.8 + "px",
+                    width:
+                        windowSize.height > windowSize.width
+                            ? windowSize.width * 0.8 + "px"
+                            : "auto",
+                }}
+            >
+                {clicked ? (
+                    <></>
+                ) : (
+                    <img
+                        className="kokomi"
+                        alt="kokomi"
+                        src={KOKOMI_IMAGE}
+                    ></img>
+                )}
+            </div>
+            <div className="numbers">{"KOKOMI COUNT : " + value}</div>
+        </div>
+    );
 }
 
 export default App;
