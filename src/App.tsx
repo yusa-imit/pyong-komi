138292ae05d36ae253352c3201b0018d4119f67a
yusa-imit <esimileiar81@gmail.com>
2022-02-23 13:05:10 +0900

    KOKOMI

--- src/App.tsx
+++ src/App.tsx
@@ -4,59 +4,66 @@ import axios from "axios";
 import kokomiVideo from "./images/kokomi.mp4";
 
 interface formatData {
-  value: number;
+    value: number;
 }
 
 function App() {
-  const [value, setValue] = useState(-1);
-  const video = useRef<HTMLVideoElement>(null);
+    const [value, setValue] = useState(-1);
+    const video = useRef<HTMLVideoElement>(null);
 
-  useEffect(() => {
-    (async () => {
-      const res = await axios.get<formatData>(
-        "https://api.countapi.xyz/get/kokomi.sutora.org/1e72fd45-9e3d-422a-8559-4ebf25288917"
-      );
+    useEffect(() => {
+        (async () => {
+            const res = await axios.get<formatData>(
+                "https://api.countapi.xyz/get/kokomi.sutora.org/1e72fd45-9e3d-422a-8559-4ebf25288917"
+            );
 
-      setValue(res.data.value);
-    })();
-  }, []);
-
-  useEffect(() => {
-      if (video.current) {
-          video.current.play();
-      }
-  }, [video]);
-
-  return (
-    <div
-      className="App"
-      onClick={() => {
-        if (video.current) {
-          video.current.currentTime = 0;
-          video.current.play();
-        }
-
-        axios
-          .get<formatData>(
-            "https://api.countapi.xyz/hit/kokomi.sutora.org/1e72fd45-9e3d-422a-8559-4ebf25288917"
-          )
-          .then((res) => {
             setValue(res.data.value);
-          });
-      }}
-    >
-      <div
-        className="kokomi-container"
-        style={{
-          height: "80%",
-          width: "80%",
-        }}
-      >
-        <video className="kokomi" src={kokomiVideo} ref={video} autoPlay playsInline muted/>
-      </div>
-      <div className="numbers">{"KOKOMI COUNT : " + value}</div>
-    </div>
-  );
+        })();
+    }, []);
+
+    useEffect(() => {
+        if (video.current) {
+            video.current.play();
+        }
+    }, [video]);
+
+    return (
+        <div
+            className="App"
+            onClick={() => {
+                if (video.current) {
+                    video.current.currentTime = 0;
+                    video.current.play();
+                }
+
+                axios
+                    .get<formatData>(
+                        "https://api.countapi.xyz/hit/kokomi.sutora.org/1e72fd45-9e3d-422a-8559-4ebf25288917"
+                    )
+                    .then((res) => {
+                        setValue(res.data.value);
+                    });
+            }}
+        >
+            <div
+                className="kokomi-container"
+                style={{
+                    height: "80%",
+                    width: "80%",
+                }}
+            >
+                <video
+                    className="kokomi"
+                    src={kokomiVideo}
+                    ref={video}
+                    autoPlay
+                    playsInline
+                    muted
+                />
+            </div>
+            <div className="numbers">{"KOKOMI COUNT : " + value}</div>
+        </div>
+    );
 }
 
 export default App;
